c98b5a7dd06dea82684dbd18833520262ce150df
donglai.zang <952388326@qq.com>
2020-07-25 23:51:06 +0800

    "auto dispose develop"

--- Demo/app/src/main/java/com/example/firstapple/MainActivity.kt
+++ Demo/app/src/main/java/com/example/firstapple/MainActivity.kt
@@ -3,30 +3,20 @@ package com.example.firstapple
 import android.accounts.NetworkErrorException
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.uber.autodispose.AutoDispose
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.observers.DefaultObserver
+import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     val live = MutableLiveData<Boolean>()
@@ -40,40 +30,37 @@ class MainActivity : AppCompatActivity() {
         }
         live.value = true
         live.observe(this, Observer {
-            Log.e("mytest","live data onChanged ")
+            Log.e("mytest", "live data onChanged ")
         })
 
     }
 
     val cacheObservable = Observable.fromCallable {
 //        Thread.sleep(500)
-        Log.e("mytest", " 缓存加载数据成功 " )
+        Log.e("mytest", " 缓存加载数据成功 ")
         " 从缓存中加载数据"
     }.subscribeOn(Schedulers.io())
 
     val netObservable = Observable.fromCallable {
         throw (NetworkErrorException())
         Thread.sleep(2000)
-        Log.e("mytest", " 网络拉取数据成功 " )
+        Log.e("mytest", " 网络拉取数据成功 ")
         " 从网络中加载数据"
     }.subscribeOn(Schedulers.computation())
 
 
-
-
-
-    fun getCacheOb():Observable<String>{
+    fun getCacheOb(): Observable<String> {
         return Observable.create(object : ObservableOnSubscribe<String> {
             override fun subscribe(it: ObservableEmitter<String>) {
 //            Thread.sleep(500)
-                Log.e("mytest","")
+                Log.e("mytest", "")
                 it.onNext(" 从缓存中加载数据")
                 it.onComplete()
             }
         }).subscribeOn(Schedulers.io())
     }
 
-    fun getNetOb():Observable<String>{
+    fun getNetOb(): Observable<String> {
         return Observable.create(object : ObservableOnSubscribe<String> {
             override fun subscribe(it: ObservableEmitter<String>) {
                 throw(Error())
@@ -88,9 +75,10 @@ class MainActivity : AppCompatActivity() {
     fun concatTest() {
         Schedulers.io().createWorker()
         // Note that onError notifications will cut ahead of onNext notifications on the emission
-        val list = listOf(cacheObservable,netObservable)
-        Observable.concat(getCacheOb(),getNetOb()).observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object :Observer<String>{
+        val list = listOf(cacheObservable, netObservable)
+        Observable.concat(getCacheOb(), getNetOb())
+            .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<String> {
                 override fun onComplete() {
                 }
 
@@ -98,18 +86,26 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onNext(t: String) {
-                    Log.e("mytest","  onNext  "+t)
+                    Log.e("mytest", "  onNext  " + t)
                 }
 
                 override fun onError(e: Throwable) {
-                    Log.e("mytest","  onError  "+e)
+                    Log.e("mytest", "  onError  " + e)
                 }
 
             })
     }
 
-    fun test(a:String?){
-        a?.substring(1..2)
+    fun test(a: String?) {
+        Observable.create<String> { }.`as`(
+            AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(this, Lifecycle.Event.ON_DESTROY))
+        )
+            .subscribe({
+
+            }, {
+
+            })
+
     }
 
 }
